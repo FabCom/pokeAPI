a09d3c3215444c909c0484ce55f8912ad57df193
Got <blondelgautier@gmail.com>
2021-10-13 17:20:04 +0200

    gamefreaks nique ta mère

--- lib/app.rb
+++ lib/app.rb
@@ -16,52 +16,54 @@ def get_cards array
   return hand
 end
 
-def display_pokemon (object_pokemon)
-  puts object_pokemon.name
+def get_pokemon_stats (poke_object)
+  poke_stats = {
+    "pokemon_name": poke_object.name,
+    "#{poke_object.stats[1].stat.name}" => poke_object.stats[1].base_stat,
+    "#{poke_object.stats[2].stat.name}" => poke_object.stats[2].base_stat
+  }
+  return poke_stats
 end
 
-def get_pokemon_stats (object_pokemon)
-  object_pokemon.stats.each do |i|
-    print i.stat.name + " : " + i.base_stat.to_s + "|" 
-  end
-  puts
-end
-
-def display_hands (hand)
+def fighter_hand (hand)
+  current_hand = []
   hand.each do |i|
-  display_pokemon(i)
-  get_pokemon_stats(i)
+    current_hand.push(get_pokemon_stats(i))
+  end
+  return current_hand
+end
+
+def compare_stats(player_poke, cpt_poke)
+  player_health = (cpt_poke["attack"].to_i - player_poke["defense"].to_i)
+  cpt_health = (player_poke["attack"].to_i - cpt_poke["defense"].to_i)
+  puts player_health 
+  puts cpt_health 
+  if player_health > cpt_health
+    puts "tu lui as niqué sa race"
+  elsif player_health < cpt_health
+    puts "il te traite de salop"
+  else puts "vos parents vous forcent à vous serrer la main, tu lui fais un fuck en partant"  
   end
 end
 
-def delete_pokemon (hand, number_pokemon)
-  hand.delete_at(number_pokemon - 1)
-end
-
-def delete_3_pokemon(hand)
-  puts "Choissisez parmis les 6 pokemon, 3 pokemon a retirer"
+def game_start(func)
+  hand_player = func.slice(0, 3)
+  computer_player = func.slice(3, func.size - 1)
   
-  while hand.length != 3
-    input = gets.chomp.to_i
-    while (input > hand.length && input < 1)
-      puts "Choisissez parmis 1 et #{hand.length}}"
-      input = gets.chomp.to_i
-    end
-    delete_pokemon(hand, input)
+  puts "Choose a character MAN"
+  puts "Enter the position of the Pokemon you want"
+
+  hand_player.each_with_index do |x, i| 
+    puts "Fighter: type #{i + 1} for #{x[:pokemon_name].capitalize}"
   end
+  position = gets.chomp.to_i
+  pl_curr_fight = hand_player[position - 1]
+  cpt_curr_fight = computer_player[rand(0..computer_player.length - 1)]
+  compare_stats(pl_curr_fight, cpt_curr_fight)
+  game_start(fighter_hand(get_cards(random_6_number)))
 end
 
-hand_player = get_cards(random_6_number)
-computer_hand = get_cards(random_6_number)
-
-
-
-puts "choice 3 from 6 pokemon"
-
-delete_3_pokemon(hand_player)
-display_hands(hand_player)
-
-puts hand_player[0].stats[]
-
+puts "Hahaha je suis Sacha du bourg Palette, je te défie !!! Pikachu attaque éclair !!!"
+game_start(fighter_hand(get_cards(random_6_number)))
 
 
