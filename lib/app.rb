cd36069c49db2f5401673625227fd52aae081404
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-13 20:09:46 +0200

    refactoring part2

--- lib/app.rb
+++ lib/app.rb
@@ -1,33 +1,46 @@
 require 'poke-api-v2'
 
+
+
 def api_get_pokemon(id) # returns a POKEMON OBJECT via its id number
   return PokeApi.get(pokemon: id)
 end
 
-def create_hand(nb_cards) # returns a random HAND
-  hand = Array.new
-  nb_cards.times do hand.push(api_get_pokemon( rand(1..99) )) end
-  return hand
-end
-
-def get_pokemon_stats(pokemon) # returns a HASH of the stats of a pokemon
+def get_poke_stats(poke) # returns a HASH of the stats of a pokemon
   { 
-    "pokemon_name": pokemon.name,
-    "#{pokemon.stats[1].stat.name}" => pokemon.stats[1].base_stat,
-    "#{pokemon.stats[2].stat.name}" => pokemon.stats[2].base_stat
+    "pokemon_name": poke.name,
+    "#{poke.stats[1].stat.name}" => poke.stats[1].base_stat,
+    "#{poke.stats[2].stat.name}" => poke.stats[2].base_stat
   }
 end
 
-def fighter_hand (nb_cards) # 
-  hand = create_hand(nb_cards)
-  hand.map {|i| get_pokemon_stats(i)}
+
+def welcome()
+  puts puts
+  puts "Hahaha je suis Sacha du bourg Palette, je te défie !!!" 
+  sleep 1
+  puts "Pikachu attaque éclair !!!"
+  puts puts
+  sleep 0.5
+end
+
+def create_hand(nb_cards) # returns a random HAND of pokemons
+  hand = Array.new
+  nb_cards.times do hand.push(api_get_pokemon( rand(1..99) )) end
+  hand.map {|i| get_poke_stats(i)}
+end
+
+
+
+
+
+def display_poke(poke)
+  puts "#{poke[:pokemon_name].capitalize} : FORCE(#{poke["attack"]}) | DÉFENSE(#{poke["defense"]})"
 end
 
 def compare_stats(player_poke, cpt_poke, match_results)
   player_health = (player_poke["defense"] - cpt_poke["attack"])
   cpt_health = (cpt_poke["defense"] - player_poke["attack"])
-  # puts player_health
-  # puts cpt_health
   puts puts
   if player_health > cpt_health
     puts "Tu lui as fait mordre la poussière ! Bon travail :-)"
@@ -42,10 +55,6 @@ def compare_stats(player_poke, cpt_poke, match_results)
   sleep 1.5
   system('clear')
   return match_results
-end
-
-def display_poke(poke)
-  puts "#{poke[:pokemon_name].capitalize} : FORCE(#{poke["attack"]}) | DÉFENSE(#{poke["defense"]})"
 end
 
 def wins_report (match_results)
@@ -70,41 +79,40 @@ def wins_report (match_results)
   end
 end
 
+
 def game_start()
-  hand_player = fighter_hand(3)
-  computer_player = fighter_hand(3)
+  player_hand = create_hand(3)
+  computer_hand = create_hand(3)
   match_results = [0, 0, 0]
 
-  while hand_player.length > 0
-    puts puts
+  while player_hand.length > 0
     puts "Voici ton équipe"
-    hand_player.each {|i| display_poke(i)}
-    position_computer_poke = rand(0..computer_player.length - 1)
-    cpt_curr_fight = computer_player[position_computer_poke]
+    player_hand.each {|pokemon| display_poke(pokemon)}
+    position_computer_poke = rand(0..computer_hand.length - 1)
+    cpt_curr_fight = computer_hand[position_computer_poke]
     puts puts
     puts "Tu vas affronter : "
     puts display_poke(cpt_curr_fight)
     puts puts
     puts "Choisis un poke, mec, en entrant sa position dans ton terminal de geek :"
-    hand_player.each_with_index do |x, i|
+    player_hand.each_with_index do |x, i|
       puts "Fighter: type #{i + 1} for #{x[:pokemon_name].capitalize}"
     end
     position = gets.chomp.to_i
-    pl_curr_fight = hand_player[position - 1]
+    pl_curr_fight = player_hand[position - 1]
 
     match_results = compare_stats(pl_curr_fight, cpt_curr_fight, match_results)
-    hand_player.delete_at(position - 1)
-    computer_player.delete_at(position_computer_poke)
+    player_hand.delete_at(position - 1)
+    computer_hand.delete_at(position_computer_poke)
   end
   wins_report(match_results)
 end
 
 def process
-  puts puts
-  puts "Hahaha je suis Sacha du bourg Palette, je te défie !!! Pikachu attaque éclair !!!" 
-  puts puts
-  sleep 1.5
-  game_start()
+  system('clear')
+  welcome
+  game_start
+  results
 end
 
 process
